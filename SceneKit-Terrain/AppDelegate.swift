60b88bf3fab0d1308da7f3b110dac0a89a7167f6
Vivek Nagar <nagarvivek00@gmail.com>
2018-09-06 20:24:16 -0500

    Initial Commit

---
+++ SceneKit-Terrain/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  SceneKit-Terrain
+//
+//  Created by Vivek Nagar on 9/6/18.
+//  Copyright © 2018 Vivek Nagar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
